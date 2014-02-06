5c2cfac924a05d0effb9152ce65f3c53ab45941d
Guilherme <guilherme@guilherme-VirtualBox.(none)>
2014-02-06 00:20:23 -0200

    aula

--- py_mongo.py
+++ py_mongo.py
@@ -3,7 +3,13 @@ import pymongo
 connection = pymongo.MongoClient("mongodb://guipoa:coursetest@ds027749.mongolab.com:27749/course")
 db = connection.course
 
-name = {"name":"guilherme"}
+#name = {"name":"guilherme"}
+#db.names.insert(name);
 
-db.names.insert(name);
+# handle to names collection
+names = db.names
+
+item = names.find_one()
+
+print item['name']
 
