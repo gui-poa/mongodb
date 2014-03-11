0dffdf1dfe17a4830b87708e3465db5da0a5c91f
Guilherme Souza <guilherme.souzac@corp.terra.com.br>
2014-03-11 21:51:35 +0000

    week5 homeworks

--- dev/week5/homework/5-2.js
+++ dev/week5/homework/5-2.js
@@ -1,13 +1,10 @@
-db.zips.aggregate([
-{
-	 _id: { state: "$state", city: "$city" },
-                           popSum: { $sum: "$pop" } 
-}
-,
-{
-        $match: {state:"CT",pop:{$gt:25000}}
-},
-{	
-	$group: {_id: "$_id.city",avgCity:{$avg: "$popSum"}}
-}
-])
+db.zips.aggregate( { $group :
+                         { _id : { state : "$state", city : "$city" },
+                           pop : { $sum : "$pop" } } },
+                        {$match: {"state":{$in:["CA, NY"]},"pop":{$gt:25000}}},
+                       { $group :
+                       { _id : "$_id.state",
+                         avgCityPop : { $avg : "$pop" } } },
+			{$sort : {_id:1}}
+)
+
