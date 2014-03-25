e83902ba4a79f3bb5cce720a0814c633eeed3ca2
Guilherme <guilherme@guilherme-VirtualBox.(none)>
2014-03-25 01:58:05 -0300

    exericio 7

--- dev/exam/7/script.py
+++ dev/exam/7/script.py
@@ -3,22 +3,26 @@
 #db.images.find({tags:{$in:["kittens"]}}).count()
 #db.albums.find({images:{$in:[10000]}})
 #db.images.find({_id:10000})
+#db.albums.ensureIndex({tags:1})
 import pymongo
-
+import sys
 def using_set():
 
    #connection = pymongo.Connection("mongodb://guipoa:senhabanco@ds027779.mongolab.com:27779/m101")	
     connection = pymongo.Connection("mongodb://localhost")
     db=connection.m101
     images = db.images
-
+    albums = db.albums
 
     try:
         # get the doc
-        score = images.find({}).count()
-        print "Total: ", score
-
-        # update using set
+	a=0
+        cursor = images.find()
+	for post in cursor:
+		c = albums.find({"images":{"$in":[post['_id']]}}).count()
+		if c == 0:
+		   images.remove({"_id":post['_id']})
+	
 	#scores.update({'student':1, 'type':'exam'},{'$set':{'examiner':'Jones'}})
 
         #score = scores.find_one({'student':1, 'type':'exam'})
